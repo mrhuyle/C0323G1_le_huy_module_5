b5e7bb833708bb79dbec0f00ffe86992781e4aa7
Le Huy <lehuy@Les-MacBook-Air.local>
2023-08-18 21:09:28 +0700

    ss2 login

--- ss2/login/src/components/Home.js
+++ ss2/login/src/components/Home.js
@@ -1,3 +1,25 @@
 import React, { Component } from "react";
 
-class Home extends React.Component {}
+class Home extends React.Component {
+  componentWillUnmount() {
+    alert("Good bye!!!");
+  }
+  render() {
+    return (
+      <div className="container d-flex align-items-center text-center">
+        <div className="form-signin">
+          <h1 className="h3 mb-3 fw-normal">Welcome</h1>
+          <button
+            className="w-100 btn btn-lg btn-primary"
+            type="button"
+            onClick={this.props.onLogOut}
+          >
+            Log out
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
