71ead0dd06fcaf30a99a82be0e8ea6d28a0e887a
mrhuyle <mrhuyle@gmail.com>
2023-08-23 11:51:43 +0700

    ss3 todo

--- ss3_component/todo/src/App.js
+++ ss3_component/todo/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      item: "",
+    };
+  }
+  handleChange = (event) => {
+    this.setState({
+      item: event.target.value,
+    });
+    console.log(this.state.item);
+  };
+
+  handleAddItem = () => {
+    let inputValue = document.getElementById("input").value;
+    console.log(inputValue);
+    if (!this.state.list.includes(inputValue)) {
+      this.setState({
+        item: "",
+        list: [...this.state.list, inputValue],
+      });
+      console.log(this.state.list);
+    }
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <input
+          id="input"
+          type="text"
+          onChange={this.handleChange.bind(this)}
+          name="item"
+        />
+        <button onClick={this.handleAddItem.bind(this)}>Add</button>
+        <ul>
+          {this.state.list.map((data, key) => (
+            <li key={key}>{data}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
