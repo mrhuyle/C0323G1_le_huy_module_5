e4d3ab423f121b48781af4fa084f7b9756fccd14
mrhuyle <mrhuyle@gmail.com>
2023-08-24 22:21:51 +0700

    ss6 book

--- ss6_api/book/src/App.js
+++ ss6_api/book/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Book } from "./components/Book";
+import { Header } from "./components/Header";
+import * as bookService from "./services/BookService";
 
 function App() {
+  // Book list state
+  const [bookList, setBookList] = useState({});
+
+  // Get all function
+  const getAll = async () => {
+    const response = await bookService.getAll();
+    console.log(response);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <table>
+        <thead>
+          <th>Title</th>
+          <th>Quantity</th>
+          <th>Actions</th>
+        </thead>
+      </table>
+    </>
   );
 }
 
---
+++ ss6_api/book/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="Header">
+      <h2>Library</h2>
+      <button className="add_btn">Add a new Book</button>
+    </div>
+  );
+};
