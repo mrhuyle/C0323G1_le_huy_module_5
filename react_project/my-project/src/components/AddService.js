94142aa5de0faa22f20d01aa6a46303c5c416ee9
mrhuyle <mrhuyle@gmail.com>
2023-08-30 11:01:04 +0700

    case render form

--- react_project/my-project/src/components/AddService.js
+++ react_project/my-project/src/components/AddService.js
@@ -1,173 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import VillaForm from "./VillaForm";
 
 const AddService = () => {
+  const [serviceType] = useState([
+    {
+      label: "Villa",
+      value: "Villa",
+      field: <VillaForm />,
+    },
+    {
+      label: "House",
+      value: "House",
+      field: <div>House</div>,
+    },
+    {
+      label: "Room",
+      value: "Room",
+      field: <div>Room</div>,
+    },
+  ]);
+  const [selectedServiceType, setSelectedServiceType] = useState(
+    serviceType[0]
+  );
+
+  const handleServiceSelect = (event) => {
+    const selectedService = serviceType.find(
+      (service) => service.value === event.target.value
+    );
+    if (selectedService) {
+      setSelectedServiceType(selectedService);
+    }
+  };
+
   return (
     <section className="w-5/12 bg-white">
       <div className="px-4 py-8 mx-auto bg-green-100 shadow-lg">
         <h2 className="mb-2 text-xl font-bold text-gray-900">
           ADD NEW SERVICE
         </h2>
-        <form action="#">
-          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-            <div className="col-span-2">
-              <div>
-                <select
-                  id="category"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
-                >
-                  <option value="Villa">Villa</option>
-                  <option value="House">House</option>
-                  <option value="Room">Room</option>
-                </select>
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                placeholder="Type service name"
-                required
-              />
-            </div>
-            <div className="w-full">
-              <label
-                htmlFor="brand"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Area (m2)
-              </label>
-              <input
-                type="number"
-                name="brand"
-                id="brand"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                placeholder="0"
-                required
-              />
-            </div>
-            <div className="w-full">
-              <label
-                htmlFor="price"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Price ($)
-              </label>
-              <input
-                type="number"
-                name="price"
-                id="price"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                placeholder="$"
-                required
-              />
-            </div>
-            <div className="w-full">
-              <label
-                htmlFor="price"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Capacity (people)
-              </label>
-              <input
-                type="number"
-                name="price"
-                id="price"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                placeholder="0"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="category"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Rent Type
-              </label>
-              <select
-                id="category"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
-              >
-                <option value="Day">Day</option>
-                <option value="Month">Month</option>
-                <option value="Year">Year</option>
-              </select>
-            </div>
-            <div>
-              <label
-                htmlFor="item-weight"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Pool (m2)
-              </label>
-              <input
-                type="number"
-                name="item-weight"
-                id="item-weight"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                placeholder={0}
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="item-weight"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Floors
-              </label>
-              <input
-                type="number"
-                name="item-weight"
-                id="item-weight"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                placeholder={0}
-                required
-              />
-            </div>
-            <div className="col-span-2">
-              <div>
-                <label
-                  htmlFor="category"
-                  className="block mb-2 text-sm font-medium text-gray-900 "
-                >
-                  Standard
-                </label>
-                <select
-                  id="category"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
-                >
-                  <option value="Villa">Standard</option>
-                  <option value="House">Deluxe</option>
-                  <option value="Room">Luxury</option>
-                </select>
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <textarea
-                id="description"
-                rows={2}
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
-                placeholder="Your description here"
-                defaultValue={""}
-              />
-            </div>
+        <div className="col-span-2 mb-5">
+          <div>
+            <select
+              id="category"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
+              onChange={handleServiceSelect}
+            >
+              {serviceType.map((type) => {
+                return (
+                  <option value={type.value} key={type.value}>
+                    {type.label}
+                  </option>
+                );
+              })}
+            </select>
           </div>
-          <button
-            type="submit"
-            className="mx-1 inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 "
-          >
-            Add service
-          </button>
-          <button
-            type="reset"
-            className="mx-1  inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 "
-          >
-            Cancel
-          </button>
-        </form>
+        </div>
+        {setSelectedServiceType && selectedServiceType.field}
       </div>
     </section>
   );
